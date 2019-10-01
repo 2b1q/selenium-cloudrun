7986b85108625d8a466a95d7b7886e3b6cd6fe03
2b3q <2bb3qq@gmail.com>
2019-10-01 11:39:12 +0300

    upd atsndalone container

--- selenium.client.js
+++ selenium.client.js
@@ -9,7 +9,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 // SELENIUM_BROWSER=chrome:36:LINUX \
 // SELENIUM_REMOTE_URL=http://www.example.com:4444/wd/hub \
 class Driver {
-  constructor(browser = 'chrome', server = 'http://localhost:4444/wd/hub') {
+  constructor(browser = 'chrome', server = 'https://selenode-debug-7cpfp2vfoa-ew.a.run.app/wd/hub') {
     this.driver = new Builder()
       .forBrowser(browser)
       .usingServer(server)
---
+++ standalone/redirect.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/redir :8080 127.0.0.1:4444
